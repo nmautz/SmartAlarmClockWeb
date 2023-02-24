120a096c2ea6399316b59b83b6771d5058ea7418
nmautz <nmautz@zagmail.gonzaga.edu>
2023-02-23 16:28:51 -0800

    server

--- server.js
+++ server.js
@@ -3,10 +3,42 @@ express = require('express');
 app = express();
 app.set('view engine', 'ejs');
 
+alarm = {
+
+    time: {
+        hour: 7,
+        minute: 30,
+        meridium: "AM"
+    },
+
+    song: {
+        name: "song.mp3",
+        cover_src: "https://media.timeout.com/images/102187439/image.jpg"
+    }
+
+
+}
+
 
 app.get('/', function(req, res) {
     console.log("Serving index")
     res.render('index', {title: 'Hello World!'})
 });
+
+app.post('/update_server', (req, res)=>{
+
+    console.log("Updating server")
+    alarm = req.body
+    res.send("Alarm updated")
+
+
+})
+
+app.post('/update_client', (req, res)=>{
+
+    console.log("Updating client")
+    res.send(alarm)
+
+})
 
 app.listen(3000);
